55306cdcd26da9b2a7e5977b6d72eb186b073b30
ShreyanshNarayan <sriansh.ara@gmail.com>
2023-02-13 23:30:36 +0530

    updating cards code

---
+++ cards.php
@@ -0,0 +1,197 @@
+<div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?fashion" alt="fashion">
+                    <div class="card-body">
+                        <p class="card-text">Fashion</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?mobiles" alt="Mobile Phones">
+                    <div class="card-body">
+                        <p class="card-text">Mobile Phones</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?laptop" alt="Laptops">
+                    <div class="card-body">
+                        <p class="card-text">Laptops</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php?laptops/" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?cars" alt="cars">
+                    <div class="card-body">
+                        <p class="card-text">Cars</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?Motorcycles" alt="motorcycles">
+                    <div class="card-body">
+                        <p class="card-text">Motorcycles</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?Furniture" alt="Furniture">
+                    <div class="card-body">
+                        <p class="card-text">Furniture</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?Bed" alt="Home accessories">
+                    <div class="card-body">
+                        <p class="card-text">Home accesories</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?shoes" alt="shoes">
+                    <div class="card-body">
+                        <p class="card-text">Shoes</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?container" alt="containers">
+                    <div class="card-body">
+                        <p class="card-text">Boxes and containers</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?bicycles" alt="cycles">
+                    <div class="card-body">
+                        <p class="card-text">Cycles</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?electronics" alt="electronics">
+                    <div class="card-body">
+                        <p class="card-text">Electronics</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?books" alt="">
+                    <div class="card-body">
+                        <p class="card-text">Books and stationary</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?keyboard/mouse" alt="">
+                    <div class="card-body">
+                        <p class="card-text">Keyboards and mouses</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="https://source.unsplash.com/random/900*100/?buckets/mugs/bathroom" alt="">
+                    <div class="card-body">
+                        <p class="card-text">Bathroom accesories</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/crud/goods.php" class=""> <button type="button"
+                                        class="btn btn-sm btn-outline-dark">View</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
